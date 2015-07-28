VanirAOSP/frameworks_base

fix cursor leaks in F_B
my previous commit was submitted before i was finished.  Here is an updated version.

PS2: missing bracket

Change-Id: I9d8048e49695cdac7ded086cd49ee6eb99e518cc

core/java/android/provider/CallLog.java
@@ -318,26 +318,26 @@ public class CallLog {
 
                 // We should prefer normalized one (probably coming from
                 // Phone.NORMALIZED_NUMBER column) first. If it isn't available try others.
-                if (ci.normalizedNumber != null) {
+		        if (ci.normalizedNumber != null) {
                     final String normalizedPhoneNumber = ci.normalizedNumber;
                     cursor = resolver.query(Phone.CONTENT_URI,
-                            new String[] { Phone._ID },
-                            Phone.CONTACT_ID + " =? AND " + Phone.NORMALIZED_NUMBER + " =?",
-                            new String[] { String.valueOf(ci.person_id), normalizedPhoneNumber},
-                            null);
+                        new String[] { Phone._ID },
+                        Phone.CONTACT_ID + " =? AND " + Phone.NORMALIZED_NUMBER + " =?",
+                        new String[] { String.valueOf(ci.person_id), normalizedPhoneNumber},
+                        null);
                 } else {
                     final String phoneNumber = ci.phoneNumber != null ? ci.phoneNumber : number;
                     cursor = resolver.query(
-                            Uri.withAppendedPath(Callable.CONTENT_FILTER_URI,
-                                    Uri.encode(phoneNumber)),
-                            new String[] { Phone._ID },
-                            Phone.CONTACT_ID + " =?",
-                            new String[] { String.valueOf(ci.person_id) },
-                            null);
+                        Uri.withAppendedPath(Callable.CONTENT_FILTER_URI,
+                                Uri.encode(phoneNumber)),
+                        new String[] { Phone._ID },
+                        Phone.CONTACT_ID + " =?",
+                        new String[] { String.valueOf(ci.person_id) },
+                        null);
                 }
-
+                
                 if (cursor != null) {
-                    try {
+					try {
                         if (cursor.getCount() > 0 && cursor.moveToFirst()) {
                             final Uri feedbackUri = DataUsageFeedback.FEEDBACK_URI.buildUpon()
                                     .appendPath(cursor.getString(0))
@@ -348,6 +348,7 @@ public class CallLog {
                         }
                     } finally {
                         cursor.close();
+
                     }
                 }
             }

ShreyasKorgaonkar/x_android_framework_base

core/java/android/provider/CallLog.java
@@ -538,27 +538,26 @@ public class CallLog {
 
                 // We should prefer normalized one (probably coming from
                 // Phone.NORMALIZED_NUMBER column) first. If it isn't available try others.
-                if (ci.normalizedNumber != null) {
+		        if (ci.normalizedNumber != null) {
                     final String normalizedPhoneNumber = ci.normalizedNumber;
                     cursor = resolver.query(Phone.CONTENT_URI,
-                            new String[] { Phone._ID },
-                            Phone.CONTACT_ID + " =? AND " + Phone.NORMALIZED_NUMBER + " =?",
-                            new String[] { String.valueOf(ci.contactIdOrZero),
-                                    normalizedPhoneNumber},
-                            null);
+                        new String[] { Phone._ID },
+                        Phone.CONTACT_ID + " =? AND " + Phone.NORMALIZED_NUMBER + " =?",
+                        new String[] { String.valueOf(ci.contactIdOrZero), normalizedPhoneNumber},
+                        null);
                 } else {
                     final String phoneNumber = ci.phoneNumber != null ? ci.phoneNumber : number;
                     cursor = resolver.query(
-                            Uri.withAppendedPath(Callable.CONTENT_FILTER_URI,
-                                    Uri.encode(phoneNumber)),
-                            new String[] { Phone._ID },
-                            Phone.CONTACT_ID + " =?",
-                            new String[] { String.valueOf(ci.contactIdOrZero) },
-                            null);
+                        Uri.withAppendedPath(Callable.CONTENT_FILTER_URI,
+                                Uri.encode(phoneNumber)),
+                        new String[] { Phone._ID },
+                        Phone.CONTACT_ID + " =?",
+                        new String[] { String.valueOf(ci.contactIdOrZero) },
+                        null);
                 }
-
+                
                 if (cursor != null) {
-                    try {
+					try {
                         if (cursor.getCount() > 0 && cursor.moveToFirst()) {
                             final Uri feedbackUri = DataUsageFeedback.FEEDBACK_URI.buildUpon()
                                     .appendPath(cursor.getString(0))
@@ -569,6 +568,7 @@ public class CallLog {
                         }
                     } finally {
                         cursor.close();
+
                     }
                 }
             }